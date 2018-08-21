9081a5ada0131dbfedc555e11e11e0fc770c944e
Lanil Marasinghe <lanil.marasinghe@pearson.com>
2018-08-21 17:20:07 +0530

    Basic setup complete

---
+++ index.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const uuidv1 = require('uuid/v1');
+const uuidv4 = require('uuid/v4');
+const cookieParser = require('cookie-parser');
+const nocache = require('nocache');
+
+const app = express();
+
+const PORT = 3001;
+
+// Applying middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(nocache());
+
+// Views
+app.use(express.static('views'));
+
+// Server Startup
+app.listen(PORT, () => {
+    console.log(`Double Submit Cookies Pattern Demo Started On ${PORT}`);
+});
+
+// JS Object to store Session IDs with CSRF tokens
+const SESSION_IDS = {};
+
+// Login Page Load
+app.get('/', (req, res) => {
+
+    const sessionID = req.cookies['session-id'];
+    if (sessionID && SESSION_IDS[sessionID]) {
+        console.log("Login: Valid Session Found !");
+        res.sendFile('views/form.html', {root: __dirname});
+    } else {
+        console.log("Login: No Valid Session Found !");
+        res.sendFile('views/login.html', {root: __dirname});
+    }
+});
+
+// Validate Credentials
+app.post('/home', (req, res) => {
+
+    const username = req.body.inputUsername;
+    const password = req.body.inputPassword;
+
+    if (username === 'root' && password === 'root') {
+
+        console.log("Home: Logged with valid credentials");
+
+        // Generating Session ID and Token
+        const SESSION_ID = uuidv1();
+        const CSRF_TOKEN = uuidv4();
+
+        console.log(`Generated Session ID: ${SESSION_ID}, CSRF Token: ${CSRF_TOKEN}`);
+
+        // Setting Cookie on Header
+        res.setHeader('Set-Cookie', [`session-id=${SESSION_ID}`, `time=${Date.now()}`, `csrf-token=${CSRF_TOKEN}`]);
+
+        res.sendFile('views/form.html', {root: __dirname});
+    } else {
+        const error = {status: 401, message: 'Invalid Credentials'};
+        res.sendFile('views/form-error.html', {root: __dirname});
+    }
+
+});
+
+// Submit Form Data
+app.post('/posts', (req, res) => {
+
+    const inputTitle = req.body.inputTitle;
+    const inputContent = req.body.inputContent;
+
+    const inputToken = req.body.inputToken;
+    const cookieToken = req.cookies['csrf-token'];
+
+    // Checking if Cookie Token matches CSRF Token Submitted
+    if (cookieToken === inputToken) {
+        console.log("Post Content: Valid CSRF Tokens Received !");
+        res.sendFile('views/form-success.html', {root: __dirname});
+    } else {
+        console.error("Post Content: No Valid CSRF Tokens Received ! !");
+        res.sendFile('views/form-error.html', {root: __dirname});
+    }
+
+});
+
+// Signs out and clear the session ID with CSRF token
+app.post('/logout', (req, res) => {
+
+    const sessionID = req.cookies['session-id'];
+
+    console.log(sessionID + ': Removed');
+
+    res.clearCookie("session-id");
+    res.clearCookie("time");
+    res.clearCookie("csrf-token");
+
+    res.sendFile('views/login.html', {root: __dirname});
+});
+
+// When user exciplity load home page URL
+app.get('/home', (req, res) => {
+
+    const sessionID = req.cookies['session-id'];
+    if (sessionID && SESSION_IDS[sessionID]) {
+        console.log("GET /home: Valid Session Found !");
+        res.sendFile('views/form.html', {root: __dirname});
+    } else {
+        console.log("GET /home: No Valid Session Found !");
+        res.sendFile('views/login.html', {root: __dirname});
+    }
+
+});
+
+// When user exciplity load logout page URL
+app.get('/logout', (req, res) => {
+    res.redirect('/');
+});
+
+// respond with "hello world" when a GET request is test route
+app.get('/health', function (req, res) {
+    res.send('Welcome to Synchronize Token Pattern Demo !')
+});
